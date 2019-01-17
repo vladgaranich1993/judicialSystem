de91600cb395eaaae2089da9a4bd221b64f97bb8
vladgaranich <vladislav.garanich@gmail.com>
2019-01-17 19:37:25 +0200

    the first commit

---
+++ app/script.js
@@ -0,0 +1,7 @@
+
+document.querySelector("#menu-button").addEventListener("click", function () {
+    document.querySelector(".mobile").classList.toggle("active");
+});
+document.querySelector("#js-close-header-menu").addEventListener("click", function () {
+    document.querySelector(".mobile").classList.toggle("active");
+});
